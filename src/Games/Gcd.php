ba7f0d815f93574976aa1090d7a008a3386c6fa7
Ivan Kryukovskiy <froscrain@gmail.com>
2025-08-05 03:34:23 +0300

    Add brain-gcd game

---
+++ src/Games/Gcd.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace BrainGames\Games\Gcd;
+
+use function BrainGames\Engine\startGame;
+
+use const BrainGames\Engine\ROUNDS_COUNT;
+
+const DESCRIPTION = 'Find the greatest common divisor of given numbers.';
+
+function getGcd($number1, $number2): int
+{
+    while ($number2 !== 0) {
+        $remainder = $number1 % $number2;
+        $number1 = $number2;
+        $number2 = $remainder;
+    }
+
+    return $number1;
+}
+
+function prepareGameData(): array
+{
+    $data = [];
+
+    for ($i = 1; $i <= ROUNDS_COUNT; $i++) {
+        $number1 = random_int(1, 100);
+        $number2 = random_int(1, 100);
+
+        $question = "{$number1} {$number2}";
+        $answer = (string) getGcd($number1, $number2);
+
+        $data[] = [$question, $answer];
+    }
+
+    return $data;
+}
+
+function startBrainGcdGame(): void
+{
+    $data = prepareGameData();
+
+    startGame($data, DESCRIPTION);
+}
